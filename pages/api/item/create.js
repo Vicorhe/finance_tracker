a43e94e2f3c083e2eeb4470fdf6cf9b593bbe919
Victor He <vctr.he@gmail.com>
2021-04-09 07:41:30 -0700

    pull institution name on creation

--- pages/api/item/create.js
+++ pages/api/item/create.js
@@ -3,7 +3,7 @@ import { query } from '../../../lib/db'
 
 export default async function handler(req, res) {
   const { publicToken, user_id } = req.body;
-  const response = await client
+  var response = await client
     .exchangePublicToken(publicToken)
     .catch((err) => {
       if (!publicToken) {
@@ -12,12 +12,33 @@ export default async function handler(req, res) {
       console.log(err)
     });
   const { access_token, item_id } = response
+
+  response = await client
+    .getItem(access_token)
+    .catch((err) => {
+      if (!access_token) {
+        return "no access token";
+      }
+      console.log(err)
+    });
+  const { item } = response
+
+  response = await client
+    .getInstitutionById(item.institution_id, ["US"])
+    .catch((err) => {
+      if (!item) {
+        return "no item found";
+      }
+      console.log(err)
+    });
+  const { institution } = response
+
   const results = await query(
     `
-    INSERT INTO items_table (plaid_item_id, access_token, user_id)
-    VALUES (?, ?, ?)
+    INSERT INTO items_table (plaid_item_id, access_token, user_id, institution_name)
+    VALUES (?, ?, ?, ?)
     `,
-    [item_id, access_token, user_id]
+    [item_id, access_token, user_id, institution.name]
   )
-  return res.send({access_token: response.access_token});
+  return res.send({ access_token: response.access_token });
 }

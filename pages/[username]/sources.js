bef5873da7aad9453f7dea4e45e34aed84b4b2b0
Victor He <vctr.he@gmail.com>
2021-04-10 15:59:45 -0700

    added base component to accounts page

--- pages/[username]/sources.js
+++ pages/[username]/sources.js
@@ -1,13 +1,23 @@
 import { useContext, useEffect, useState } from 'react'
 import axios from 'axios'
 import { UserContext } from '../../context'
+import { mutate } from 'swr'
 import { useAccounts, useItems } from '../../lib/swr-hooks'
-import { Box } from "@chakra-ui/react"
+import {
+  Box, Accordion, AccordionItem,
+  AccordionButton, AccordionPanel, AccordionIcon,
+  Button
+} from "@chakra-ui/react"
 import Nav from '../../components/Nav'
 import PlaidLink from "../../components/PlaidLink"
 import utilStyles from '../../styles/utils.module.scss'
+import LoadingError from '../../components/LoadingError'
+import LoadingList from '../../components/LoadingList'
 
 export default function Account() {
+  const { accounts, isAccountsError } = useAccounts();
+  const { items, isItemsError } = useItems();
+
   const [token, setToken] = useState(null)
   const [access_token, setAccessToken] = useState(null)
   const { user } = useContext(UserContext)
@@ -31,6 +41,31 @@ export default function Account() {
     const res = await axios.post('http://localhost:3000/api/item/create', { publicToken: publicToken, user_id: user.id });
     const data = res.data.access_token;
     setAccessToken(data)
+    mutate('/api/item/get-all')
+    mutate('/api/account/get-all')
+  }
+
+  function SourcesTable() {
+    return (
+      <Accordion allowMultiple>
+        {items.filter(i => i.user_id == user.id).map(i => (
+          <AccordionItem key={i.id}>
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">{i.institution_name}</Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel flex flexDirection="column" pb={4}>
+              {accounts.filter(a => a.item_id == i.id).map(a => (
+                <p key={a.id}>{a.name}</p>
+              ))}
+              <Button ml="auto" colorScheme="red">Delete</Button>
+            </AccordionPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    )
   }
 
   return (
@@ -43,12 +78,13 @@ export default function Account() {
             createItem={createItem}
           />}
       </Nav>
-      <h1>Sources Page </h1>
-      <div>
-        <p>Token: {token}</p>
-        <p>Access Token: {access_token}</p>
-
-      </div>
+      {
+        (isAccountsError || isItemsError)
+          ? LoadingError()
+          : (!accounts || !items)
+            ? LoadingList()
+            : SourcesTable()
+      }
     </Box>
   )
 }

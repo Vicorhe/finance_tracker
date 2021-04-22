d33ec54c1a66f86bfea1eada25dc68e2f05cdecd
Victor He <vctr.he@gmail.com>
2021-04-22 08:27:09 -0700

    added other fields, still populating fields according to selected object

--- components/EditTransactionModal.js
+++ components/EditTransactionModal.js
@@ -1,4 +1,5 @@
-import { useState, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import DatePicker from "../components/DatePicker";
 import {
   useDisclosure,
   Modal,
@@ -11,7 +12,10 @@ import {
   FormControl,
   FormLabel,
   Input,
-  IconButton,
+  NumberInput,
+  NumberInputField,
+  Select,
+  Textarea,
   Spacer,
   AlertDialog,
   AlertDialogBody,
@@ -20,17 +24,34 @@ import {
   AlertDialogContent,
   AlertDialogOverlay,
 } from "@chakra-ui/react"
+import { useAreas } from '../lib/swr-hooks'
 import { mutate } from 'swr'
+import moment from 'moment';
 
 export default function EditTransactionModal({ transaction, isModalOpen, onModalClose }) {
-  const { isOpen: isAlertOpen, onOpen: onAlertOpen, onClose: onAlertClose } = useDisclosure(
-    {
-      
-    })
+  const {
+    isOpen: isAlertOpen,
+    onOpen: onAlertOpen,
+    onClose: onAlertClose
+  } = useDisclosure()
   const [name, setName] = useState('')
+  const [amount, setAmount] = useState('')
+  const [area, setArea] = useState('')
+  const [date, setDate] = useState(new Date())
+  const [memo, setMemo] = useState('')
   const [submitting, setSubmitting] = useState(false)
+  const { areas, isAreasError } = useAreas();
   const cancelRef = useRef()
   const [deleting, setDeleting] = useState(false)
+
+  useEffect(() => {
+    setName(transaction.name)
+    setAmount(69)//transaction.amount)
+    console.log(typeof(transaction.amount))
+    setArea(transaction.area)
+    setDate(new Date(transaction.date))
+    setMemo(transaction.memo ? transaction.memo : '')
+  }, [transaction])
 
   async function submitHandler(e) {
     setSubmitting(true)
@@ -86,12 +107,59 @@ export default function EditTransactionModal({ transaction, isModalOpen, onModal
           <form onSubmit={submitHandler}>
             <ModalCloseButton />
             <ModalBody pb={6}>
-              <FormControl my="1.15rem">
-                <FormLabel>Transaction name</FormLabel>
+              <FormControl mb="4">
+                <FormLabel>Name</FormLabel>
                 <Input
-                  placeholder="Transaction name"
-                  value={transaction.name}
+                  placeholder="Paradise Zoanthids"
+                  value={name}
                   onChange={(e) => setName(e.target.value)}
+                />
+              </FormControl>
+              <FormControl mb="4">
+                <FormLabel>Amount</FormLabel>
+                <NumberInput precision={2}>
+                  <NumberInputField
+                    placeholder="6.17"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                  />
+                </NumberInput>
+              </FormControl>
+              {
+                !!areas &&
+                <FormControl mb="4">
+                  <FormLabel>Area</FormLabel>
+                  <Select
+                    value={area}
+                    onChange={(e) => setArea(e.target.value)}
+                  >
+                    {
+                      areas.map((a) =>
+                        <option key={a.id}
+                          value={a.id}>{a.name}
+                        </option>
+                      )
+                    }
+                  </Select>
+                </FormControl>
+              }
+
+              <FormControl mb="4">
+                <FormLabel htmlFor="date">Date</FormLabel>
+                <DatePicker
+                  id="date"
+                  selectedDate={date}
+                  onChange={d => setDate(d)}
+                  showPopperArrow={true}
+                />
+              </FormControl>
+
+              <FormControl mb="4">
+                <FormLabel>Memo</FormLabel>
+                <Textarea
+                  placeholder="Rewarding myself"
+                  value={memo}
+                  onChange={(e) => setMemo(e.target.value)}
                 />
               </FormControl>
             </ModalBody>

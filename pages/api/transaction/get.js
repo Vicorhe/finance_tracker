1900dcd85dc73f29f33d751fa40208a00cd8fde3
Victor He <vctr.he@gmail.com>
2021-04-29 07:54:32 -0700

    added and tested get single transaction

---
+++ pages/api/transaction/get.js
@@ -0,0 +1,21 @@
+import { query } from '../../../lib/db'
+
+export default async function handler(req, res) {
+  const { id } = req.query
+  try {
+    if (!id) {
+      return res
+        .status(400)
+        .json({ message: '`id` is required' })
+    }
+    const results = await query(
+      `
+      SELECT * FROM transactions_table 
+      WHERE id = ?
+      `,
+      id)
+    return res.json(results)
+  } catch (e) {
+    res.status(500).json({ message: e.message })
+  }
+}

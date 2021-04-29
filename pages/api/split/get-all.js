1467bea2f9808ada0cb320a7b2e34c113644d9b6
Victor He <vctr.he@gmail.com>
2021-04-29 07:57:26 -0700

    added get all split transactions by parent, tested

---
+++ pages/api/split/get-all.js
@@ -0,0 +1,21 @@
+import { query } from '../../../lib/db'
+
+export default async function handler(req, res) {
+  const { parent_id } = req.query
+  try {
+    if (!parent_id) {
+      return res
+        .status(400)
+        .json({ message: '`parent_id` is required' })
+    }
+    const results = await query(
+      `
+      SELECT * FROM transactions_table 
+      WHERE parent_id = ?
+      `,
+      parent_id)
+    return res.json(results)
+  } catch (e) {
+    res.status(500).json({ message: e.message })
+  }
+}

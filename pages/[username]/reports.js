0685201018952ed8e8e9f0f434c5560016672a0e
Victor He <vctr.he@gmail.com>
2021-05-03 08:07:32 -0700

    added buttons and date selectors

--- pages/[username]/reports.js
+++ pages/[username]/reports.js
@@ -1,10 +1,19 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { UserContext } from "../../context"
 import Nav from '../../components/Nav'
-import { Box } from "@chakra-ui/react"
+import DatePicker from '../../components/DatePicker'
+import {
+  Box,
+  Flex,
+  Spacer,
+  Heading,
+  Button
+} from "@chakra-ui/react"
 import utilStyles from '../../styles/utils.module.scss'
 
 export default function Reports() {
+  const [fromDate, setFromDate] = useState(new Date())
+  const [toDate, setToDate] = useState(new Date())
   const { user } = useContext(UserContext)
   const breadcrumbs = [
     { name: user.name, path: `/${user.name}` },
@@ -14,7 +23,32 @@ export default function Reports() {
     <Box className={utilStyles.page}>
       <Nav breadcrumbs={breadcrumbs}>
       </Nav>
-      <h1>This is the Reports Page for user {user.name}</h1>
+      <Flex alignItems="center" pt="3">
+        <Heading fontSize="lg" pr="3" fontWeight="extrabold">FROM</Heading>
+        <Box width="167px">
+          <DatePicker
+            id="fromDate"
+            selectedDate={fromDate}
+            onChange={d => setFromDate(d)}
+            showPopperArrow={true}
+          />
+        </Box>
+        <Heading fontSize="lg" px="3" fontWeight="extrabold">TO</Heading>
+        <Box width="167px">
+          <DatePicker
+            id="toDate"
+            selectedDate={toDate}
+            onChange={d => setToDate(d)}
+            showPopperArrow={true}
+          />
+        </Box>
+        <Spacer/>
+        <Button
+        colorScheme="messenger"
+        >
+          Generate Report
+          </Button>
+      </Flex>
     </Box>
   )
 }

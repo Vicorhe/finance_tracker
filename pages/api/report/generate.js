5bfdcb4e712e5797b2e817a908a56342ae70699f
Victor He <vctr.he@gmail.com>
2021-05-06 07:31:09 -0700

    added areas aggregate query

--- pages/api/report/generate.js
+++ pages/api/report/generate.js
@@ -1,7 +1,7 @@
 import { query } from '../../../lib/db'
 
 export default async function handler(req, res) {
-  const { 
+  const {
     user_id,
     start_date,
     end_date
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
         .status(400)
         .json({ message: '`user_id`, `start_date`, and `end_date` are required' })
     }
-    const results = await query(
+    const transactions = await query(
       `
       SELECT * FROM transactions_table
       WHERE user_id = ? AND date >= '`+ start_date + `' AND date <= '` + end_date + `' 
@@ -20,7 +20,17 @@ export default async function handler(req, res) {
         AND (split = false OR (split = true AND parent_id IS NOT NULL))
       `,
       user_id)
-    return res.json(results)
+    const areas_aggregate = await query(
+      `
+      SELECT A.id id, A.name label, SUM(T.amount) value, COUNT(T.id) count, A.description, A.income 
+      FROM transactions_table T
+        INNER JOIN areas_table A ON T.area_id = A.id 
+      WHERE user_id = ? AND date >= '`+ start_date + `' AND date <= '` + end_date + `' 
+        AND hidden = false AND (split = false OR (split = true AND parent_id IS NOT NULL))
+      GROUP BY A.id
+      `,
+      user_id)
+    return res.json({ transactions, areas_aggregate })
   } catch (e) {
     res.status(500).json({ message: e.message })
   }

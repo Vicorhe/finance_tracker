2dd0dbfb071cab68c665b88d0d554c90100f986e
Victor He <vctr.he@gmail.com>
2021-05-11 08:13:48 -0700

    finalized bar chart look

--- components/BarChart.js
+++ components/BarChart.js
@@ -27,23 +27,35 @@ export default function BarChart({ data, fromDate, toDate }) {
     router.push(`/${user.name}/reports/breakdown`)
   }
 
+  function ToolTip({ id, value, color }) {
+    return (
+      <Box p={3}>
+        <Text fontSize="xl">
+          {id}: ${value}
+        </Text>
+        <Text fontSize="xs">
+          Click for Breakdown
+        </Text>
+      </Box>
+    );
+  }
+
   return <ResponsiveBar
     data={data}
-    keys={['hot dog', 'burger']}
-    indexBy="country"
-    margin={{ top: 50, right: 30, bottom: 150, left: 30 }}
+    keys={['primary', 'secondary']}
+    indexBy="area"
+    margin={{ top: 56, right: 30, bottom: 150, left: 30 }}
     innerPadding={1}
     padding={0.3}
     groupMode="grouped"
     layout="vertical"
     valueScale={{ type: 'linear' }}
     indexScale={{ type: 'band', round: true }}
-    colors={{ scheme: 'nivo' }}
-    borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+    colors={["#2d2d34", "#949d6a"]}
     axisRight={{ tickSize: 5, tickPadding: 5, tickRotation: 0, legend: '', legendOffset: 0 }}
     axisBottom={{
-      tickSize: 5,
-      tickPadding: 5,
+      tickSize: 11,
+      tickPadding: 7,
       tickRotation: 90,
       legend: '',
       legendPosition: 'middle',
@@ -57,13 +69,25 @@ export default function BarChart({ data, fromDate, toDate }) {
       legendPosition: 'middle',
       legendOffset: -40
     }}
-    enableGridX={true}
-    enableGridY={false}
-    labelSkipWidth={5}
-    labelSkipHeight={12}
-    labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-    legends={[]}
-    //tooltip={function () { }}
+    enableGridY={true}
+    labelSkipWidth={20}
+    labelTextColor="#f7f7ff"
+    legends={[
+      {
+        dataFrom: 'keys',
+        anchor: 'top-left',
+        direction: 'row',
+        justify: false,
+        translateX: -22,
+        translateY: -36,
+        itemWidth: 77,
+        itemHeight: 16,
+        itemsSpacing: 6,
+        symbolSize: 21,
+        itemDirection: 'left-to-right'
+      }
+    ]}
+    tooltip={ToolTip}
     animate={true}
     motionStiffness={90}
     motionDamping={15}

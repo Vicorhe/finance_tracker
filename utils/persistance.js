a86d646723c538e3137d1475c07cf327a485ac90
Victor He <vctr.he@gmail.com>
2021-09-09 07:56:26 -0700

    format

--- utils/persistance.js
+++ utils/persistance.js
@@ -1,7 +1,7 @@
 import { formatMySQLDate } from './date-formatter'
 
 export function setBreakdownState(areaId, breakdownStartDate, breakDownEndDate) {
-    localStorage.setItem("breakdown-start-date", formatMySQLDate(breakdownStartDate))
-    localStorage.setItem("breakdown-end-date", formatMySQLDate(breakDownEndDate))
-    localStorage.setItem("area-id", JSON.stringify(areaId))
+  localStorage.setItem("breakdown-start-date", formatMySQLDate(breakdownStartDate))
+  localStorage.setItem("breakdown-end-date", formatMySQLDate(breakDownEndDate))
+  localStorage.setItem("area-id", JSON.stringify(areaId))
 }

de38ad1b61b1868ebfe93b2c2a15a909e9d65b79
Victor He <vctr.he@gmail.com>
2021-09-14 09:42:23 -0700

    removed user context in project

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,13 @@
 import '../styles.scss'
-import { useState } from 'react'
 import { ChakraProvider, Container } from '@chakra-ui/react';
-import { UserContext } from '../context'
 import '../styles/date-picker.css'
 
 function MyApp({ Component, pageProps }) {
-  const [user, setUserState] = useState({})
-
-  function setUser(a) {
-    setUserState(a)
-  }
-
   return (
     <ChakraProvider>
-      <UserContext.Provider value={{ user, setUser }}>
-        <Container maxW={{ base: "container.lg", "2xl": "96rem" }} p="2rem">
-          <Component {...pageProps} />
-        </Container>
-      </UserContext.Provider>
+      <Container maxW={{ base: "container.lg", "2xl": "96rem" }} p="2rem">
+        <Component {...pageProps} />
+      </Container>
     </ChakraProvider>
   );
 }
